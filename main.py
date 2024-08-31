e4e626fdae9a5ddd1e2328f8b9cb31f6ec0ffab7
FoRPoP <bapop413@gmail.com>
2024-08-31 19:21:36 +0200

    feat: Use multiple GANs

--- main.py
+++ main.py
@@ -1,48 +1,44 @@
+import torch
 from classifier import *
 from gan import *
 
 def main():
 
-    gan = prepare_gan()
-
-    # Generisanje i prikazivanje slika nakon treniranja
-    #gan.sample_images(epoch=50)
-
-    # Generisanje dataset-a
-    images, labels = [], []
-    for i in range(10):
-        images_i, labels_i = gan.generate_dataset(n=1000, label=i)  # Generišemo 500 slika sa labelom i
-        images.extend(images_i)
-        labels.extend(labels_i)
+    images, labels = generate_synthetic_data()
 
     mnist_classifier = MNISTClassifier(lr=0.001, input_dim=784, output_dim=10, hidden_dim=300, dropout_rate=0.5)
-    #train_loader, validation_loader, test_loader = mnist_classifier.load_and_preprocess_data(validation_split=0.2)
+    train_loader, validation_loader, test_loader = mnist_classifier.load_and_preprocess_data(validation_split=0.2)
     train_loader, validation_loader, test_loader = mnist_classifier.load_and_preprocess_data(train_data=images, train_labels=labels, validation_split=0.2)
-    mnist_classifier.train_model(train_loader=train_loader, validation_loader=validation_loader, num_epochs=50)
-    true_labels, pred_labels = mnist_classifier.evaluate_model(test_loader)
+    mnist_classifier.train_model(train_loader=train_loader, validation_loader=validation_loader, num_epochs=100)
 
+    torch.save(mnist_classifier, 'saved_models/classifier.pth')
+
+    true_labels, pred_labels = mnist_classifier.evaluate_model(test_loader)
     mnist_classifier.plot_confusion_matrix(true_labels, pred_labels)
 
-def prepare_gan() -> GAN:
+def generate_synthetic_data() -> Tuple[list, list]:
 
-    # Učitavanje MNIST podataka
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
-    ])
+    images, labels = [], []
 
-    mnist_data = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
-    data_loader = DataLoader(mnist_data, batch_size=64, shuffle=True)
+    # Dobijanje DataLoader-a za svaki broj
+    digit_loaders = get_digit_data_loaders()
 
-    # Postavljanje uređaja (CPU ili GPU)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Treniranje GAN-a za svaki broj i generisanje datasetova
+    for digit, loader in digit_loaders.items():
+        print(f"Training GAN for digit {digit}")
+        gan = GAN()
+        gan.to(gan.device)
+        gan.train(loader, num_epochs=10)
 
-    # Treniranje GAN modela
-    gan = GAN().to(device)
-    gan.train(data_loader, num_epochs=50)
+        # Generisanje 1000 slika za trenutni broj
+        images_digit, labels_digit = gan.generate_dataset(n=1000, label=digit)
+        
+        images.extend(images_digit)
+        labels.extend(labels_digit)
 
-    return gan
+        torch.save(gan, f'saved_models/gan_{digit}.pth')
 
+    return images, labels
 
 if __name__ == "__main__":
     main()
